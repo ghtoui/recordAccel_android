3781c1f671476143a11519113ae57f7b5fef1bb1
ghtoui <92294389+ghtoui@users.noreply.github.com>
2023-12-18 18:04:11 +0900

    edge to edgeとMaterial Theme の実装

--- app/src/main/java/com/moritoui/recordaccel/MainActivity.kt
+++ app/src/main/java/com/moritoui/recordaccel/MainActivity.kt
@@ -3,7 +3,9 @@ package com.moritoui.recordaccel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,12 +16,15 @@ import com.moritoui.recordaccel.ui.theme.RecordAccelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             RecordAccelTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
