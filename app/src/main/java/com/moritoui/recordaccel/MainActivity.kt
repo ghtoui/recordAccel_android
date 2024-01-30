3ff26eb2436802bd4987ea64081fd1b2a8f3cd3d
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-01-30 14:32:39 +0900

    feat: 加速度を取得し続け、ViewModelからViewに加速度を表示できるように :tada:

--- app/src/main/java/com/moritoui/recordaccel/MainActivity.kt
+++ app/src/main/java/com/moritoui/recordaccel/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.moritoui.recordaccel
 
+import MainScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,16 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.moritoui.recordaccel.ui.theme.RecordAccelTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         setContent {
             RecordAccelTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,25 +28,9 @@ class MainActivity : ComponentActivity() {
                         .safeDrawingPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RecordAccelTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/moritoui/recordaccel/viewModel/MainScreenViewModel.kt
@@ -0,0 +1,37 @@
+package com.moritoui.recordaccel.viewModel
+
+import android.util.Log
+import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.moritoui.recordaccel.model.MotionSensor
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainScreenViewModel @Inject constructor(
+    val motionSensor: MotionSensor
+) : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            while(true) {
+                checkAccDataList()
+            }
+        }
+    }
+
+    fun getAccData(): String {
+        return motionSensor.getAccData()
+    }
+
+    private suspend fun checkAccDataList() {
+        delay(1000)
+        val accList = motionSensor.getAccDataList()
+        Log.d("sensor", accList.size.toString())
+    }
+}
