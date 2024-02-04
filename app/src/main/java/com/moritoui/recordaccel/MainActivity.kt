82ebad9da0d4e65b79a22c8f6235886a912b74ad
ghtoui <92294389+ghtoui@users.noreply.github.com>
2024-02-04 15:26:35 +0900

    fix: edgeToEdgeができていなかったので、修正 :bug:
    safeDrawingPadding()の位置が悪かった。

--- app/src/main/java/com/moritoui/recordaccel/MainActivity.kt
+++ app/src/main/java/com/moritoui/recordaccel/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.moritoui.recordaccel.navigation.Navigation
 import com.moritoui.recordaccel.ui.theme.RecordAccelTheme
@@ -21,15 +18,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             RecordAccelTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .safeDrawingPadding(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Navigation()
-                }
+                Navigation(
+                    modifier = Modifier.safeDrawingPadding()
+                )
             }
         }
     }
